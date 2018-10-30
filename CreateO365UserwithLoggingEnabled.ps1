6fac91f15167d9f51b8fecebf70af105d5ca3b66
Randombuffalo <kritter@amtci.org>
2018-10-30 09:46:56 -0500

    added Sign-in functions

--- CreateO365UserwithLoggingEnabled.ps1
+++ CreateO365UserwithLoggingEnabled.ps1
@@ -1,11 +1,42 @@
+#Checks to see if we are already connected.
+function isMSOLConnected {
+    Get-MsolDomain -ErrorAction SilentlyContinue | out-null
+    $result = $?
+    return $result
+}
+
+#first, Connect to exchange online and Office 365
+Function connectToMSOL{    
+    $UserCredential = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection -ErrorAction stop
+    Import-PSSession $Session
+    Connect-MsolService -Credential $UserCredential
+    }
+
+if (-not (isMSOLConnected)) {
+    ConnectToMSOL
+}
+
 #Set License pack options
 $Standardlicense = New-MsolLicenseOptions -AccountSkuId "amtci:STANDARDWOFFPACK" -DisabledPlans "YAMMER_ENTERPRISE", "INTUNE_O365", "SWAY", "FLOW_O365_P1", "POWERAPPS_O365_P1", "TEAMS1", "PROJECTWORKMANAGEMENT"
 $mailboxcheck=$false
+$usercheck=$false
 #search for user and assign License pack
-$userupn = read-Host "Enter the UPN of the user you would like to assign licenses"
-Write-host "Assigning Standard License options to the user " + $userupn
-Set-MsolUserLicense -UserPrincipalName $userupn -AddLicenses "amtci:STANDARDWOFFPACK" -LicenseOptions $Standardlicense
-
+while ($usercheck -eq $false){    
+    $userupn = read-Host "Enter the UPN of the user you would like to assign licenses"
+    try {
+    Write-host "Assigning Standard License options to the user " + $userupn
+    Set-MsolUserLicense -UserPrincipalName $userupn -AddLicenses "amtci:STANDARDWOFFPACK" -LicenseOptions $Standardlicense -UsageLocation US -ErrorAction Stop
+    }
+    catch {
+    write-host "Error setting user license"
+    $error
+    }
+    if(!$error){
+    Write-Host "User license assigned"
+    $usercheck=$true
+    }
+}
 #Wait for mailbox creation
 "Waiting for mailbox to be created..."
 Start-sleep -Seconds 15
@@ -16,7 +47,7 @@ while($mailboxcheck -eq $false){
         Get-Mailbox -Identity $userupn -ErrorAction stop
         }
     catch { 
-        "Error occured, waiting 60 seconds to try again..."
+        write-host "Error occured, waiting 60 seconds to try again..."
         Start-sleep -seconds 60
         }
     if (!$error) {
